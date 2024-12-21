5b5d357e1838665d5694370e8b641450d1d103ea
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-12-21 20:46:10 +0530

    Update setup.sh
    fixed stuff

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -6,43 +7,51 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 WHITE='\033[0;37m'
 RESET='\033[0m'
+
 # log timestamps
-timestamp() { date +"%Y-%m-%d %H:%M:%S" }
+timestamp() { date +"%Y-%m-%d %H:%M:%S"; }
+
 # creating configuration file
+mkdir -p src
 touch src/configuration.json
+
 # starting setup process
-echo -e "
-                       /$$                        
-                      | $$                        
-  /$$$$$$$  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$ 
- /$$_____/ /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$
-|  $$$$$$ | $$$$$$$$  | $$    | $$  | $$| $$  \ $$
- \____  $$| $$_____/  | $$ /$$| $$  | $$| $$  | $$
- /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$/
-|_______/  \_______/   \___/   \______/ | $$____/ 
-                                        | $$      
-                                        | $$      
-                                        |__/      
-"
-echo -e "${GREEN}Welcome to the setup process for your project"
-echo -e "${YELLOW}All the dependencies will be installed automatically."
-echo -e "${YELLOW}Eventually, you will be guided to type some information to fill the configuration file."
-echo -e "${GREEN}Please note that what you type here is not uploaded to anywhere or seen by anyone except yourself!\n"
+echo -e "${BLUE}
+    ███████╗███████╗████████╗██╗   ██╗██████╗ 
+    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+    ███████╗█████╗     ██║   ██║   ██║██████╔╝
+    ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+    ███████║███████╗   ██║   ╚██████╔╝██║     
+    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
+${RESET}"
+
+echo -e "${GREEN}Welcome to the setup process for your project${RESET}"
+echo -e "${YELLOW}All the dependencies will be installed automatically.${RESET}"
+echo -e "${YELLOW}Eventually, you will be guided to type some information to fill the configuration file.${RESET}"
+echo -e "${GREEN}Please note that what you type here is not uploaded to anywhere or seen by anyone except yourself!\n${RESET}"
+
 # installing deps
-echo -e "${BLUE}$(timestamp) :: Installing dependencies…"
+echo -e "${BLUE}$(timestamp) :: Installing dependencies...${RESET}"
 npm i github:tryforge/forgescript github:tryforge/forgedb sqlite3 
-echo -e "${BLUE}$(timestamp) :: Finishing up…"
+echo -e "${BLUE}$(timestamp) :: Finishing up...${RESET}"
 npm cache clean --force
-echo -e "${GREEB}$(timestamp) :: Successfully installed all the dependencies!"
+echo -e "${GREEN}$(timestamp) :: Successfully installed all the dependencies!${RESET}"
+
 # beginning of config file setup
-echo -e "${WHITE}Starting configuration file setup…"
-echo -e "${YELLOW}Info: currently you can only have one prefix!"
+echo -e "${WHITE}Starting configuration file setup...${RESET}"
+echo -e "${YELLOW}Info: currently you can only have one prefix!${RESET}"
+
 # creating config file
-> src/configuration.json
-# getting data from user
 read -p "App Token: " -e A
 read -p "App Prefix: " -e B
-echo -e "{\ntoken=\"${A}\",\nprefix=[\"${B}\"]\n}" >> configuration.json
-echo -e "\n${GREEN}$(timestamp) :: Successfully set up configuration file."
-echo -e "${YELLOW}Keep this file safe in a lcoal folder!"
-echo -e "${RED}Do not share this file to anyone!"
+
+cat > src/configuration.json << EOF
+{
+    "token": "${A}",
+    "prefix": ["${B}"]
+}
+EOF
+
+echo -e "\n${GREEN}$(timestamp) :: Successfully set up configuration file.${RESET}"
+echo -e "${YELLOW}Keep this file safe in a local folder!${RESET}"
+echo -e "${RED}Do not share this file to anyone!${RESET}"
